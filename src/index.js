25181501234cdacc2ac8e2cf1258c4ad32a28478
Konstantin Alekseev <lexxxlexus@mail.ru>
2020-09-12 00:03:17 +0600

    added context, bug fixed

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import * as serviceWorker from './serviceWorker';
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./Redux/reduxStore";
-import StoreContext from "./storeContext.js";
 import {BrowserRouter} from "react-router-dom";
+import StoreContext from "./storeContext";
 
 let rerenderAllTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <StoreContext.Provider value={store}>\
-                        <App />
+                <StoreContext.Provider value={store}>
+                    <App />
                 </StoreContext.Provider>
             </BrowserRouter>
-            document.getElementById('root')
         </React.StrictMode>,
+        document.getElementById('root')
     );
 }
 
