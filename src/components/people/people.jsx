45bf44fa33442ecf4f8205ea6176222bdbc703f8
Konstantin Alekseev <lexxxlexus@mail.ru>
2020-09-21 09:13:50 +0600

    added class component to people page

--- src/components/people/people.jsx
+++ src/components/people/people.jsx
@@ -3,52 +3,59 @@ import classes from './people.module.css';
 import * as axios from 'axios'
 import userDeafaultAva from "../../../src/assets/images/noava.jpg"
 
-const People = (props) => {
+class People extends React.Component{
 
-    if (props.peopleList.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            debugger
-            props.setusers(response.data.items)
-        });
+    getPeopleList = () => {
+        if (this.props.peopleList.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                debugger
+                this.props.setusers(response.data.items)
+            });
+        }
     }
 
+    render() {
+        return <div className={classes.content}>
 
-    return <div className={classes.content}>
+            <div className={classes.row + " " + "row"}>
+                <div className={classes.col + " " + 'col-xs-1'}>
+                </div>
+                <div className={classes.col + " " + 'col-xs-10'}>
 
-        <div className={classes.row + " " + "row"}>
-            <div className={classes.col + " " + 'col-xs-1'}>
-            </div>
-            <div className={classes.col + " " + 'col-xs-10'}>
-                {props.peopleList.map((user) => <div key={user.id}>
-                    <div className={classes.container}>
-                        <div>
-                            <img className={classes.avaImg} src={user.photos.small != null ? user.photos.small : userDeafaultAva} alt=""/>
+                    {this.props.peopleList.map((user) => <div key={user.id}>
+                        <div className={classes.container}>
                             <div>
-                                {user.followed ? <button className={classes.buttons + " " +  classes.unfollowButton} onClick={() => {
-                                    props.unfollow(user.id)
-                                }}> unfollow </button> : <button className={classes.buttons + " " +  classes.followButton} onClick={() => {
-                                    props.follow(user.id)
-                                }}> follow </button>}
+                                <img className={classes.avaImg}
+                                     src={user.photos.small != null ? user.photos.small : userDeafaultAva} alt=""/>
+                                <div>
+                                    {user.followed ?
+                                        <button className={classes.buttons + " " + classes.unfollowButton} onClick={() => {
+                                            this.props.unfollow(user.id)
+                                        }}> unfollow </button> :
+                                        <button className={classes.buttons + " " + classes.followButton} onClick={() => {
+                                            this.props.follow(user.id)
+                                        }}> follow </button>}
+                                </div>
+                            </div>
+                            <div className={classes.info}>
+                                <div>
+                                    <div className={classes.fullName}> {user.name}</div>
+                                    <div className={classes.status}>Status: {user.status}</div>
+                                </div>
+                                <div>
+                                    {/*<div className={classes.city}>{user.location.city}  </div>*/}
+                                    {/*<div className={classes.country}>{user.location.country}  </div>*/}
+                                </div>
                             </div>
                         </div>
-                        <div className={classes.info}>
-                            <div>
-                                <div className={classes.fullName}> {user.name}</div>
-                                <div className={classes.status}>Status: {user.status}</div>
-                            </div>
-                            <div>
-                                {/*<div className={classes.city}>{user.location.city}  </div>*/}
-                                {/*<div className={classes.country}>{user.location.country}  </div>*/}
-                            </div>
-                        </div>
-                    </div>
-
-                </div>)}
+                    </div>)}
+                    <button onClick={this.getPeopleList}> show more</button>
+                </div>
                 <div className={classes.col + " " + 'col-xs-1'}>
                 </div>
             </div>
         </div>
-    </div>
+    }
 }
 
 export default People
